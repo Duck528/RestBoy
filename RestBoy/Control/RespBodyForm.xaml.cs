92775531fec0f827e95ca9fcb5e70b01bf3f1513
Ahn <sdzaq@naver.com>
2017-02-07 10:32:24 +0900

    웹 프리뷰 경고창 제거

--- RestBoy/Control/RespBodyForm.xaml.cs
+++ RestBoy/Control/RespBodyForm.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -16,6 +18,16 @@ using System.Windows.Shapes;
 namespace RestBoy.Control
 {
     /// <summary>
+    /// thanks to this site, i can suppress warning
+    /// http://stackoverflow.com/questions/6138199/wpf-webbrowser-control-how-to-supress-script-errors
+    /// </summary>
+    [ComImport, Guid("6D5140C1-7436-11CE-8034-00AA006009FA"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IOleServiceProvider
+    {
+        [PreserveSig]
+        int QueryService([In] ref Guid guidService, [In] ref Guid riid, [MarshalAs(UnmanagedType.IDispatch)] out object ppvObject);
+    }
+    /// <summary>
     /// RespBodyForm.xaml에 대한 상호 작용 논리
     /// </summary>
     public partial class RespBodyForm : UserControl
@@ -23,6 +35,12 @@ namespace RestBoy.Control
         public RespBodyForm()
         {
             InitializeComponent();
+            this.wbPreview.Navigated += new NavigatedEventHandler(wbMain_Navigated);
+        }
+
+        void wbMain_Navigated(object sender, NavigationEventArgs e)
+        {
+            SetSilent(this.wbPreview, true); // make it silent
         }
 
         private void btnRaw_Click(object sender, RoutedEventArgs e)
@@ -35,6 +53,27 @@ namespace RestBoy.Control
         {
             this.tbxRaw.Visibility = Visibility.Collapsed;
             this.wbPreview.Visibility = Visibility.Visible;
+        }
+
+        public static void SetSilent(WebBrowser browser, bool silent)
+        {
+            if (browser == null)
+                throw new ArgumentNullException("browser");
+
+            // get an IWebBrowser2 from the document
+            IOleServiceProvider sp = browser.Document as IOleServiceProvider;
+            if (sp != null)
+            {
+                Guid IID_IWebBrowserApp = new Guid("0002DF05-0000-0000-C000-000000000046");
+                Guid IID_IWebBrowser2 = new Guid("D30C1661-CDAF-11d0-8A3E-00C04FC9E26E");
+
+                object webBrowser;
+                sp.QueryService(ref IID_IWebBrowserApp, ref IID_IWebBrowser2, out webBrowser);
+                if (webBrowser != null)
+                {
+                    webBrowser.GetType().InvokeMember("Silent", BindingFlags.Instance | BindingFlags.Public | BindingFlags.PutDispProperty, null, webBrowser, new object[] { silent });
+                }
+            }
         }
     }
 }
